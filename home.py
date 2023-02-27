95744f74a599cb58ecd8df2131b255d48a5d0c61
Viji <vijina@student.ubc.ca>
2023-02-27 14:03:59 -0800

    Homeupdates

--- home.py
+++ home.py
@@ -5,9 +5,12 @@ from dash import dcc
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import altair as alt
+from vega_datasets import data
+
 
 # loading the dataset
 bi = pd.read_csv("datasets/melted.csv")
+latlon = pd.read_csv("datasets/world_country_and_usa_states_latitude_and_longitude_values.csv")
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -40,7 +43,7 @@ app.layout = dbc.Container([
                 dcc.RangeSlider(
                     id='home_cts',
                     min=0,
-                    max=100,
+                    max=500,
                     allowCross=False,
                     tooltip={
                         'placement':'bottom'
@@ -155,28 +158,24 @@ app.layout = dbc.Container([
                 dbc.Tabs(id='tabs', children=[
                     # Home Tab
                     dbc.Tab(label='Home', children=[
-                        
-                    ]),
-                    # Resources Tab
-                    dbc.Tab(label='Resources', children=[
-                        
-                    ]),
-                    # Logistics Tab
-                    dbc.Tab(label='Logistics', children=[
-                        dbc.Row([
+                       dbc.Row([
                             dbc.Col([
-                                html.H5("Average time to clear Exports through customs (days)"),
+                                html.H5("map)"),
                                 html.Iframe(
-                                    id='cc_bar',
+                                    id='hm_map',
                                     style={'border-width': '0', 'width': '100%', 'height': '400px'}
                                 )
                             ]),
                             dbc.Col([
-                                html.H5("Logistics Performance Index")
+                                html.H5("firstchart)"),
+                                html.Iframe(
+                                    id='hm_line',
+                                    style={'border-width': '0', 'width': '100%', 'height': '400px'}
+                                )
                             ])
-                        ])
+                        ])                        
                     ])
-                    # end of logistics tab
+                    # end of home tab
                 ])
                 # end of tabs
             ])
@@ -187,27 +186,78 @@ app.layout = dbc.Container([
     # end of filters row
 ])
 
-# callback for logistics cc_bar
+# callback for home line chart
 @app.callback(
-    Output(component_id="cc_bar", component_property="srcDoc"),
+    Output(component_id="hm_map", component_property="srcDoc"),
     Input(component_id="countries", component_property="value"),
     Input(component_id="years", component_property="value"),
-    Input(component_id="logistics_cc", component_property="value")
+    Input(component_id="home_cts", component_property="value")
 )
 
-def plot_cc_bar(countries, years, logistics_cc):
+def plot_map(countries, years, home_cts):
+    #latlong data
+    # Data source - https://www.kaggle.com/datasets/paultimothymooney/latitude-and-longitude-for-every-country-and-state
+    #which is based on https://developers.google.com/public-data/docs/canonical/countries_csv
+
+    arr=home_cts
+    #data input for selected countries
 
     countries_years_series_filtered = bi[(bi['Country Name'].isin(countries)) & 
                                                 (bi['year'].isin(years)) & 
-                                                (bi['Series Name']=="Average time to clear exports through customs (days)") & 
-                                                (bi['value']<logistics_cc)]
-    chart = alt.Chart(countries_years_series_filtered).mark_bar().encode(
-        x=alt.X('Country Name'),
-        y=alt.Y('value', title='Days'),
-        color='Country Name',
-        column=alt.Column('year', title=None),
-        tooltip=['Country Name', 'year', 'value'])
+                                                (bi['Series Name']=="Cost of business start-up procedures (% of GNI per capita)") &
+                                                (bi['value']<arr[1])]
+    mergedf=pd.merge(countries_years_series_filtered, latlon, how='left',left_on='Country Name',right_on='country')
+
+    states = alt.topo_feature(data.us_10m.url, feature='states')
+    countries = alt.topo_feature(data.world_110m.url, 'countries')
+
+    background=alt.Chart(countries).mark_geoshape(
+        fill='lightgray',
+        stroke='white'
+    ).project(
+        "equirectangular"
+    ).properties(
+        width=500,
+        height=300
+    )
+
+    points = alt.Chart(mergedf).mark_circle().encode(
+        longitude='longitude:Q',
+        latitude='latitude:Q',
+        size=alt.value(10),
+        tooltip='country'
+    )
+
+    chart=background + points
+#    tooltip=xcol).interactive()
+    return chart.to_html()
+
+@app.callback(
+    Output(component_id="hm_line", component_property="srcDoc"),
+    #Output(component_id="cc_bar", component_property="srcDoc"),
+    Input(component_id="countries", component_property="value"),
+    Input(component_id="years", component_property="value"),
+   # Input(component_id="home_tts", component_property="value"),
+    Input(component_id="home_cts", component_property="value")
+   # Input(component_id="countries", component_property="value"),
+   # Input(component_id="years", component_property="value"),
+   # Input(component_id="logistics_cc", component_property="value")    
+)
+
+def plot_line(countries, years, home_cts):
+    print(countries, years, home_cts)
+    arr=home_cts
+    print("arr",type(arr))
+    countries_years_series_filtered = bi[(bi['Country Name'].isin(countries)) & 
+                                                (bi['year'].isin(years)) & 
+                                                (bi['Series Name']=="Cost of business start-up procedures (% of GNI per capita)") &
+                                                (bi['value']<arr[1])]
+    chart = alt.Chart(countries_years_series_filtered).mark_line(point=True).encode(
+        alt.X('year:O',scale=alt.Scale(zero=False),title="check"),
+        alt.Y('value:Q',scale=alt.Scale(zero=False)),
+        color=alt.Color("Country Name:N",sort="ascending",legend=alt.Legend(title="Selected Countries")),
+        tooltip='value')
     return chart.to_html()
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
